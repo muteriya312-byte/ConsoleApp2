e1807706627f268cd811e0191e17b5d3cf8bb148
DELL <DELL@DESKTOP-N0ANFHP>
2026-07-28 11:22:34 +0530

    binary_search

---
+++ binary_search.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class binary_search
+    {
+        static void Main(string[] args)
+        {
+            int i, n, search;
+            bool found = false;
+
+            Console.Write("Enter the size of Array: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter elements in sorted order:");
+
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("Enter element: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Enter element to search: ");
+            search = Convert.ToInt32(Console.ReadLine());
+
+            int low = 0;
+            int high = n - 1;
+
+            while (low <= high)
+            {
+                int mid = (low + high) / 2;
+
+                if (arr[mid] == search)
+                {
+                    Console.WriteLine("Element found at index: " + mid);
+                    found = true;
+                    break;
+                }
+                else if (search < arr[mid])
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Element not found");
+            }
+        }
+    }
+}
