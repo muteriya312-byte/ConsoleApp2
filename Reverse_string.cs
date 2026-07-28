ec615a37bb3cad224fa0942a8243bf23cc9dd4be
DELL <DELL@DESKTOP-N0ANFHP>
2026-07-28 10:16:26 +0530

    string

---
+++ Reverse_string.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Reverse_string
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String to reverse : ");
+            string str= Console.ReadLine();
+
+            string rev = "";
+            //str.Length use built in function
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            Console.WriteLine("Reversed String is :" + rev);
+
+            //without use built in function
+            //int length = 0;
+
+            //foreach (char c in str)
+            //{
+            //    //length++;
+            //    rev = c + rev;
+            //}
+            //Console.WriteLine(rev);
+
+
+        }
+    }
+}
---
+++ palindrom.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class palindrom
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String  : ");
+            string str = Console.ReadLine();
+
+            string rev = "";
+            //str.Length use built in function
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rev = rev + str[i];
+            }
+            //Console.WriteLine("Reversed String is :" + rev);
+            if (rev == str)
+            {
+                Console.WriteLine(str + " is palindrom ");
+            }
+            else
+            {
+                Console.WriteLine(str + " is NOT palindrom");
+            }
+
+        }
+    }
+}
