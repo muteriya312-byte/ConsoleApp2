5ee58f8028ac0d1ce983c4d4169e04f61635385e
DELL <DELL@DESKTOP-N0ANFHP>
2026-08-04 10:22:36 +0530

    reverse array

---
+++ reverse_array.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class reverse_array
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter a size of Array : ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+            Console.WriteLine("Enter an Array elements : ");
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int start = 0, end = n - 1;
+            while (start < end)
+            {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+
+                start++;
+                end--;
+
+            }
+            Console.WriteLine("Reversed Array is : ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
